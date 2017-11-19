b851a1a41583157abcb35dc71b42d0c962ff2e72
Raina <krisigocheva@gmail.com>
2017-11-19 10:18:18 +0200

    final version

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,29 +1,22 @@
+//Kristina Gocheva, fn 81117, group 5
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Knapsack {
+    // Parameters
     private static final int mutationPercent = 4;
 
-    private static final int populationSize = 16;
+    private static final int populationSize = 30;
 
-    private static final int maxGeneration = 7;
-
-    private static final int maxIteration = 50;
+    private static final int maxGeneration = 100;
 
     private static final double totalWeight = 5000;
 
+    //Variables
     private static List<Data> items = new ArrayList<>();
-
-    boolean block = false;
-
-    private int crossoverCount = 0;
-
-    private int cloneCount = 0;
 
     private int breed = 1;
 
@@ -39,46 +32,28 @@ public class Knapsack {
 
     private ArrayList<Boolean[]> bestGeneInGeneration = new ArrayList<>();
 
-    Knapsack() {
+    private Knapsack() {
         initialize();
         makePopulation();
-        if (!block) {
-            quantifyPopulation();
+        quantifyPopulation();
 
-            bestGeneInGeneration.add(population.get(findBestSolutionIndex()));
+        bestGeneInGeneration.add(population.get(findBestSolutionIndex(population)));
+        bestFitnessOfGeneration.add(quantifyGene(population.get(findBestSolutionIndex(population))));
 
-            bestFitnessOfGeneration.add(quantifyGene(population.get(findBestSolutionIndex())));
-            System.out.println("BEST GENE IN GENERATION : ");
-            System.out.println(printResult(bestGeneInGeneration));
-            System.out.println("WINNER");
-            bestFitnessOfGeneration.sort(Comparator.naturalOrder());
-
-            if (maxGeneration > 1) {
-                makeFurtherGenerations();
-            }
+        if (maxGeneration > 1) {
+            makeFurtherGenerations();
         }
+        System.out.println(printResult(bestGeneInGeneration));
     }
 
     public static void main(String[] args) {
         new Knapsack();
-
     }
 
     private void makePopulation() {
-        int counter = 0;
-        int terminate = 0;
-        while (counter < populationSize && terminate < maxIteration) {
+        while (population.size() < populationSize) {
             Boolean[] candidate = makeGene();
-            if (quantifyGene(candidate) != -1) {
-                population.add(candidate);
-                counter++;
-            } else {
-                terminate++;
-            }
-        }
-        if (terminate >= maxIteration) {
-            block = true;
-            System.out.println("Please restart. MaxIteration number reached.");
+            population.add(candidate);
         }
     }
 
@@ -116,35 +91,54 @@ public class Knapsack {
         if (weight <= totalWeight) {
             return value;
         }
-        return -1;
+        return 0;
     }
 
-    private int findBestSolutionIndex() {
-        int index = 0;
-        double fitness = 0;
-        double bestFitness = 0;
-        for (int i = 0; i < populationSize; i++) {
-            fitness = quantifyGene(population.get(i));
-            if (fitness > bestFitness) {
-                bestFitness = fitness;
-                index = i;
+    private void makeFurtherGenerations() {
+        for (int i = 1; i < maxGeneration; i++) {
+            for (int j = 0; j < populationSize; j++) {
+                breedPopulation();
             }
+            fitness.clear();
+            quantifyBreedPopulation();
+
+            for (int k = 0; k < populationSize; k++) {
+                if (k < breedPopulation.size()) {
+                    population.set(k, breedPopulation.get(k));
+                }
+            }
+            breedPopulation.clear();
+
+            bestGeneInGeneration.add(population.get(findBestSolutionIndex(population)));
+            bestFitnessOfGeneration.add(quantifyGene(population.get(findBestSolutionIndex(population))));
         }
-        return index;
     }
 
     private void breedPopulation() {
         int parentOneIndex;
         int parentTwoIndex;
-        breed = breed + 1;
+        breed++;
         // If population_size is odd - get best solution of previous generation
         if (populationSize % 2 == 1) {
             breedPopulation.add(bestGeneInGeneration.get(breed - 1));
         }
-        parentOneIndex = ThreadLocalRandom.current().nextInt(0, fitness.size());
-        parentTwoIndex = ThreadLocalRandom.current().nextInt(0, fitness.size());
+
+        parentOneIndex = findIndexConsideringChance();
+        parentTwoIndex = findIndexConsideringChance();
 
         crossoverGenes(parentOneIndex, parentTwoIndex);
+    }
+
+    private int findIndexConsideringChance() {
+        double random = Math.random() * generationFitness;
+        for (int i = 0; i < fitness.size(); i++) {
+            if (random <= fitness.get(i)) {
+                return i;
+            } else {
+                random -= fitness.get(i);
+            }
+        }
+        return 0;
     }
 
     private void crossoverGenes(int parentOneIndex, int parentTwoIndex) {
@@ -155,11 +149,7 @@ public class Knapsack {
         Arrays.fill(childTwo, Boolean.FALSE);
 
         double random = Math.random();
-        int terminate = 0;
-        int counter = 0;
-        double prob_crossover = 0.5;
-        while (random <= prob_crossover && terminate < maxIteration && counter < populationSize) {
-            crossoverCount = crossoverCount + 1;
+        if (random <= 0.5) {
             int crossPoint = ThreadLocalRandom.current().nextInt(0, items.size());
 
             for (int i = 0; i < crossPoint; i++) {
@@ -170,48 +160,18 @@ public class Knapsack {
                 childOne[i] = population.get(parentOneIndex)[i];
                 childTwo[i] = population.get(parentTwoIndex)[i];
             }
-            childOne = mutateGene(childOne);
-            childTwo = mutateGene(childTwo);
+            addGenesToBreedPopulation(childOne, childTwo);
+        }
+        if (random > 0.5) {
+            addGenesToBreedPopulation(population.get(parentOneIndex), population.get(parentOneIndex));
+        }
+    }
 
-            if (quantifyGene(childOne) != -1) {
-                breedPopulation.add(childOne);
-                counter++;
-            } else {
-                terminate++;
-            }
-            if (quantifyGene(childTwo) != -1) {
-                breedPopulation.add(childTwo);
-                counter++;
-            } else {
-                terminate++;
-            }
-        }
-        if (terminate >= maxIteration) {
-            block = true;
-            return;
-        }
-        terminate = 0;
-        while (random > prob_crossover && terminate < maxIteration && counter < populationSize) {
-            cloneCount = cloneCount + 1;
-
-            childOne = mutateGene(childOne);
-            childTwo = mutateGene(childTwo);
-
-            if (quantifyGene(childOne) != -1) {
-                breedPopulation.add(childOne);
-            } else {
-                terminate++;
-            }
-            if (quantifyGene(childTwo) != -1) {
-                breedPopulation.add(childTwo);
-                break;
-            } else {
-                terminate++;
-            }
-        }
-        if (terminate >= maxIteration) {
-            block = true;
-        }
+    private void addGenesToBreedPopulation(Boolean[] childOne, Boolean[] childTwo) {
+        childOne = mutateGene(childOne);
+        childTwo = mutateGene(childTwo);
+        breedPopulation.add(childOne);
+        breedPopulation.add(childTwo);
     }
 
     private Boolean[] mutateGene(Boolean[] geneWithNoMutation) {
@@ -227,42 +187,28 @@ public class Knapsack {
         return geneWithMutation;
     }
 
-    private void makeFurtherGenerations() {
-        for (int i = 1; i < maxGeneration; i++) {
-            crossoverCount = 0;
-            cloneCount = 0;
-            for (int j = 0; j < populationSize / 2; j++) {
-                breedPopulation();
-            }
-            fitness.clear();
-            quantifyBreedPopulation();
-
-            for (int k = 0; k < populationSize; k++) {
-                if (k < breedPopulation.size()) {
-                    population.set(k, breedPopulation.get(k));
-                }
-            }
-            breedPopulation.clear();
-
-            bestGeneInGeneration.add(population.get(findBestSolutionIndex()));
-            bestFitnessOfGeneration.add(quantifyGene(population.get(findBestSolutionIndex())));
-
-            System.out.println(printResult(bestGeneInGeneration));
-
-            System.out.println();
-        }
-    }
-
     private void quantifyBreedPopulation() {
         generationFitness = 0;
         for (int i = 0; i < populationSize; i++) {
             if (i < breedPopulation.size()) {
-                double temp_fitness = quantifyGene(breedPopulation.get(i));
-                fitness.add(temp_fitness);
-            } else {
-                //  fitness.add(-100.0);
+                double temp = quantifyGene(breedPopulation.get(i));
+                fitness.add(temp);
             }
         }
+    }
+
+    private int findBestSolutionIndex(List<Boolean[]> all) {
+        int index = 0;
+        double fitness = 0;
+        double bestFitness = 0;
+        for (int i = 0; i < all.size(); i++) {
+            fitness = quantifyGene(all.get(i));
+            if (fitness > bestFitness) {
+                bestFitness = fitness;
+                index = i;
+            }
+        }
+        return index;
     }
 
     private void initialize() {
@@ -285,32 +231,35 @@ public class Knapsack {
         items.add(new Data("waterproof overclothes", 430, 75));
         items.add(new Data("note-case", 220, 80));
         items.add(new Data("sunglasses", 70, 20));
-        items.add(new Data("towel", 180, 120));
+        items.add(new Data("towel", 180, 12));
         items.add(new Data("socks", 40, 50));
         items.add(new Data("book", 300, 10));
         items.add(new Data("notebook", 900, 1));
         items.add(new Data("tent", 2000, 150));
-
     }
 
     private String printResult(List<Boolean[]> generation) {
         StringBuilder sb = new StringBuilder();
-        Collections.max(bestFitnessOfGeneration);
+        Double result = Collections.max(bestFitnessOfGeneration);
         for (Boolean[] gene : generation) {
-            int weight = 0;
             int value = 0;
             for (int i = 0; i < gene.length; i++) {
                 if (gene[i]) {
-                    sb.append("1");
-                    weight += items.get(i).weight;
                     value += items.get(i).value;
-                } else {
-                    sb.append("0");
                 }
             }
-            sb.append("\tWEIGHT " + weight);
-            sb.append("\tVALUE " + value);
-            sb.append("\n");
+            if (value == result) {
+                sb.append("VALUE: ");
+                sb.append(value);
+                sb.append("\n");
+                for (int i = 0; i < gene.length; i++) {
+                    if (gene[i]) {
+                        sb.append(items.get(i).name);
+                        sb.append(" ");
+                    }
+                }
+                sb.append("\n");
+            }
         }
         return sb.toString();
     }
@@ -328,5 +277,4 @@ public class Knapsack {
             this.weight = weight;
         }
     }
-
 }
